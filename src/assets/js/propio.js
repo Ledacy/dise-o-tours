5cb6db91177ac9235495d835dbe4449cd571543f
ledacy <Ledacy.sud@gmail.com>
2022-05-31 15:52:12 -0500

    nueva version

--- src/assets/js/propio.js
+++ src/assets/js/propio.js
@@ -57,3 +57,36 @@ $('#carousel div').hover(function() {
 //  document.getElementById('#carousel div').addEventListener('hover', function() {
 //  document.getElementById('#text-1').classList.toggle('hide');
 // })
+// estilos para el header
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener('scroll', function () {
+
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+    if (window.scrollY > 650 && width > 768) {
+      document.getElementById('mainMenu').classList.add('fixed-top-wonder', 'py-0');
+      document.getElementById('sidebar').classList.add('top-lateral');
+      document.getElementById('bg').classList.remove('bg-header');
+      document.getElementById('logo-1').classList.add('d-none');
+      document.getElementById('logo-2').classList.remove('d-none');
+      document.getElementById('menu-1').classList.add('negro');
+      document.getElementById('menu-2').classList.add('negro');
+      document.getElementById('menu-3').classList.add('negro');
+      document.getElementById('menu-4').classList.add('negro');
+      document.getElementById('texto-1').classList.add('negro');
+      document.getElementById('idioma').classList.remove('text-white');
+
+    } else {
+      document.getElementById('mainMenu').classList.remove('fixed-top-wonder', 'py-3');
+      document.getElementById('sidebar').classList.remove('top-lateral');
+      document.getElementById('bg').classList.add('bg-header');
+      document.getElementById('menu-1').classList.remove('negro');
+      document.getElementById('menu-2').classList.remove('negro');
+      document.getElementById('menu-3').classList.remove('negro');
+      document.getElementById('menu-4').classList.remove('negro');
+      document.getElementById('texto-1').classList.remove('negro');
+      document.getElementById('logo-1').classList.remove('d-none');
+      document.getElementById('logo-2').classList.add('d-none');
+    }
+  });
+});
